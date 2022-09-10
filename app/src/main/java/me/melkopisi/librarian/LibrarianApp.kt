b7f54c6ba346433ad1245e224231cf134856a664
Kopisi <m.elkopisi@gmail.com>
2022-09-10 10:24:22 +0200

    built application architecture added required modules and dependencies for first feature and fix project name

---
+++ app/src/main/java/me/melkopisi/librarian/LibrarianApp.kt
@@ -0,0 +1,11 @@
+package me.melkopisi.librarian
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/*
+ * Authored by Kopisi on 10 Sep, 2022.
+ * Contact Me : m.elkopisi@gmail.com
+ */
+@HiltAndroidApp
+class LibrarianApp : Application()
